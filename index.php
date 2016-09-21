ac1ba34620f5cd9196c3d1b28b731fc6bc3d3c3e
Johnn Erick <jon91052@gmail.com>
2016-09-21 16:30:12 -0300

    projeto do felipe

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Namoradas</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="tabela">
+		<div class="list">
+
+		</div>
+		<form>
+			<div class="table">
+				<div class="left">
+					Nome da namorada
+				</div>
+				<div class="right">
+					Cor do cabelo
+				</div>
+				<div class="lista">
+					<form>
+						<div class="container-desc">
+							<div class="nome">
+								Nome
+							</div>
+							<div class="cor">
+								<input type="text" value="Morena">
+							</div>
+						</div>
+						<div class="container-desc">
+							<div class="nome">
+								Nome
+							</div>
+							<div class="cor">
+								<input type="text" value="Morena">
+							</div>
+						</div>
+						<div class="container-desc">
+							<div class="nome">
+								Nome
+							</div>
+							<div class="cor">
+								<input type="text" value="Morena">
+							</div>
+						</div>
+						<div class="button">
+							<select>
+								<option>Morena</option>
+								<option>Loira</option>
+								<option>Ruiva</option>
+							</select>
+							<input type="submit" Value="Pesquisar">
+							<input type="submit" Value="Adicionar">
+							<input type="submit" Value="Salvar edição">
+							<input type="submit" Value="Excluir">
+						</div>
+					</form>
+				</div>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
